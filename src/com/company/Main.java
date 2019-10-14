1348ce2848627d3b911396108c2c1dc0aba88e15
bert7438 <bert7438@gmail.com>
2019-10-14 19:28:58 +0300

    Solved "a+b" problem

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(a+b);
+
     }
 }
